2447ea0930cfdda53abcf6b81c254fb6f36de650
PierreSameh <captainpirzon@gmail.com>
2025-03-10 05:23:12 +0200

    fix: enhance error handling for student routes with try-catch blocks

--- src/Routers/StudentRouter.js
+++ src/Routers/StudentRouter.js
@@ -4,41 +4,61 @@ import { Student } from "../Models/Student.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const students = await Student.find();
-  res.send(students);
+  try {
+    const students = await Student.find();
+    res.send(student);
+  } catch (e) {
+    res.status(500).json({ message: `Server Error occured, ${e}` });
+  }
 });
 
 router.get("/:id", async (req, res) => {
-  const student = await Student.findById(req.params.id);
+  try {
+    const student = await Student.findById(req.params.id);
 
-  if (!student) {
-    return res.status(404);
+    if (!student) {
+      return res.status(404);
+    }
+    res.status(200).send(student);
+  } catch (e) {
+    res.status(500).json({ message: `Server Error occured, ${e}` });
   }
-  res.status(200).send(student);
 });
 
 router.post("/", async (req, res) => {
-  const student = await Student.create(req.body);
-  student.save();
-  res.status(201).send(student);
+  try {
+    const student = await Student.create(req.body);
+    student.save();
+    res.status(201).send(student);
+  } catch (e) {
+    res.status(500).json({ message: `Server Error occured, ${e}` });
+  }
 });
 
 router.put("/:id", async (req, res) => {
-  const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!student) {
-    return res.status(404);
+  try {
+    const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!student) {
+      return res.status(404);
+    }
+    res.status(200).send(student);
+  } catch (e) {
+    res.status(500).json({ message: `Server Error occured, ${e}` });
   }
-  res.status(200).send(student);
 });
 
 router.delete("/:id", async (req, res) => {
-  const student = await Student.findByIdAndDelete(req.params.id);
-  if (!student) {
-    res.status(404);
+  try {
+    const student = await Student.findByIdAndDelete(req.params.id);
+    if (!student) {
+      res.status(404);
+    }
+    res.status(200).send("Student Deleted Successfully");
+  } catch (e) {
+    res.status(500).json({ message: `Server Error occured, ${e}` });
   }
-  res.status(200).send("Student Deleted Successfully");
 });
 
 export default router;
